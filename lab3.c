86707521adc6c562a5d146827baacce1743eaa51
vadvladkis <82339626+vadvladkis@users.noreply.github.com>
2021-06-11 10:09:40 +0300

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable : 4996)
+int main(int ac, char* av[])
+{
+    FILE* fIn = fopen("old.bmp", "rb");
+    FILE* fOut = fopen("new.bmp", "wb");
+    if (!fIn || !fOut)
+    {
+        printf("File error.\n");
+        return 0;
+    }
+
+    unsigned char header[54];
+    fread(header, sizeof(unsigned char), 54, fIn);
+    fwrite(header, sizeof(unsigned char), 54, fOut);
+
+    int width = *(int*)&header[18];
+    int height = abs(*(int*)&header[22]);
+    int stride = (width * 3 + 3) & ~3;
+    int padding = stride - width * 3;
+
+    printf("Done!");
+
+    unsigned char pixel[3];
+    char gp;
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            fread(pixel, 3, 1, fIn);
+            gp = (pixel[0] + pixel[1] +pixel[2])/3;
+
+            memset(pixel, gp, sizeof(pixel));
+            fwrite(&pixel, 3, 1, fOut);
+        }
+        fread(pixel, padding, 1, fIn);
+        fwrite(pixel, padding, 1, fOut);
+    }
+    fclose(fOut);
+    fclose(fIn);
+    return 0;
+}
